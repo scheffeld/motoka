ca67c6978c0f8b1868c5b2178cd3db68ed3ca328
Roger Scheffeld <rscheffeld@gmail.com>
2020-04-06 15:56:29 -0300

    Criando tela de novo usuário.

--- src/screens/NewUser/newUser.js
+++ src/screens/NewUser/newUser.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { Form } from 'native-base';
 
 import styles from './styles'
 
+import ButtonComponent from '../../components/Button/button'
+import InputComponent from '../../components/Input/input'
+import HeaderComponent from '../../components/Header/header'
+
 const NewUser = () => {
     return(
-        <View>
-            <Text>Hello NewUser Screen.</Text>
+        <View style={styles.container}>
+            <HeaderComponent subTitle='Novo Usuário'/>
+            <View style={styles.content}>
+                <Form style={styles.form}>
+                    <View>
+                    <InputComponent
+                        label='Nome'/>
+                    <InputComponent
+                        label='Email'/>
+                    <InputComponent
+                        label='Confirmação do Email'/>
+                    <InputComponent
+                        label='Senha'/>
+                    <InputComponent
+                        label='Confirmação da Senha'/>
+                    </View>
+                </Form>
+                    
+                <ButtonComponent
+                        label='Cadastrar'
+                        type='primary'/>
+            </View>
         </View>
     )
 };
--- src/screens/NewUser/styles.js
+++ src/screens/NewUser/styles.js
@@ -1,7 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#FFF'
+    },
+    form: {
+        marginLeft: -10,
+        marginTop: 30
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'space-between'
+    }
 });
 
 export default styles;
