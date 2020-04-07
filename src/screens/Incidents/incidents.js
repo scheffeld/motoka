c8f044c76a796cfa98e67d59b0fc209a00ff3fce
Roger Scheffeld <rscheffeld@gmail.com>
2020-04-07 18:52:38 -0300

    Alterando propriedades na pagina de incidentes.

--- src/screens/Incidents/incidents.js
+++ src/screens/Incidents/incidents.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles'
 
+import HeaderComponent from '../../components/Header/header'
+
 const Incidents = () => {
+    const navigation = useNavigation()
     return(
-        <View>
-            <Text>Hello Incidents Screen.</Text>
+        <View style={styles.container}>
+            <HeaderComponent
+                subTitle='Registros'
+                iconName='log-out'
+                onPress={() => navigation.navigate('Login')}/>
+            <FlatList
+                data={[1, 2, 3]}
+                style={styles.listContainer}
+                showsVerticalScrollIndicator={false}
+                renderItem={({item: incident }) => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>TIPO: </Text>
+                        <Text style={styles.incidentValue}>Abastecimento</Text>
+                        <Text style={styles.incidentProperty}>DATA:</Text>
+                        <Text style={styles.incidentValue}>06/04/2020</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => navigation.navigate('Details')}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Icon name='chevron-right' color='#E02041' size={16}/>
+                        </TouchableOpacity>
+                    </View>
+                )}/>
         </View>
     )
 };
--- src/screens/Incidents/styles.js
+++ src/screens/Incidents/styles.js
@@ -1,7 +1,42 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    container: {    
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: '#FFF'
+    },
+    listContainer: {
+        marginTop: 20
+    },
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#F0F0F0',
+        marginBottom: 16
+    },
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414D',
+        fontWeight: 'bold',
+    },
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    detailsButtonText: {
+        color: '#E02041',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
 });
 
 export default styles;
